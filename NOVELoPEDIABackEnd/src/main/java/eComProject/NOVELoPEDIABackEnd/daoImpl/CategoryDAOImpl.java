32f06fd0a5c51e07d62399ca9a6466ba58fe98b3
Sangeetha-BalaKrishnan <sangeethakrish5797@gmail.com>
2017-08-14 18:17:07 +0530

    Successfully tested CRUD operations

--- NOVELoPEDIABackEnd/src/main/java/eComProject/NOVELoPEDIABackEnd/daoImpl/CategoryDAOImpl.java
+++ NOVELoPEDIABackEnd/src/main/java/eComProject/NOVELoPEDIABackEnd/daoImpl/CategoryDAOImpl.java
@@ -1,9 +1,9 @@
 package eComProject.NOVELoPEDIABackEnd.daoImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,68 +11,32 @@ import org.springframework.transaction.annotation.Transactional;
 import eComProject.NOVELoPEDIABackEnd.dao.CategoryDAO;
 import eComProject.NOVELoPEDIABackEnd.dto.Category;
 
-
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
 
 	@Autowired
 	public SessionFactory sessionFactory;
 
-	@Autowired
-	private static List<Category> categories = new ArrayList<>();
-
-	static {
-		Category category = new Category();
-		// Adding category
-		category.setId(1);
-		category.setName("Tv");
-		category.setDescription("This is some discription");
-		category.setImageURL("cat.png");
-
-		categories.add(category);
-
-		// 2
-
-		category = new Category();
-		// Adding category
-		category.setId(2);
-		category.setName("Mobile");
-		category.setDescription("This is some discription");
-		category.setImageURL("cat2.png");
-
-		categories.add(category);
-
-		// 3
-
-		category = new Category();
-		// Adding category
-		category.setId(3);
-		category.setName("lap");
-		category.setDescription("This is some discription");
-		category.setImageURL("cat3.png");
-
-		categories.add(category);
-	}
-
 	@Override
 	public List<Category> list() {
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
 
-		return categories;
+		query.setParameter("active", true);
+
+		return query.getResultList();
 	}
 
+	// Getting Single Category based on iD
 	@Override
 	public Category get(int id) {
 
-		// Enhanced For loop
-		for (Category cat : categories) {
-			if (cat.getId() == id)
-				return cat;
-		}
-		return null;
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 	@Override
-	@Transactional
+
 	public boolean add(Category category) {
 
 		try {
@@ -87,6 +51,35 @@ public class CategoryDAOImpl implements CategoryDAO {
 		}
 
 	}
-	
+
+	// Update A single Category
+	@Override
+	public boolean update(Category category) {
+		try {
+
+			sessionFactory.getCurrentSession().update(category);
+
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+
+		}
+
+	}
+
+	@Override
+	public boolean delete(Category category) {
+		category.setActive(false);
+		try {
+			sessionFactory.getCurrentSession().update(category);
+
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+
+		}
+	}
 
 }
--- NOVELoPEDIABackEnd/src/test/java/eComProject/NOVELoPEDIABackEnd/test/CategoryTestCase.java
+++ NOVELoPEDIABackEnd/src/test/java/eComProject/NOVELoPEDIABackEnd/test/CategoryTestCase.java
@@ -1,6 +1,5 @@
 package eComProject.NOVELoPEDIABackEnd.test;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
@@ -10,41 +9,102 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import eComProject.NOVELoPEDIABackEnd.dao.CategoryDAO;
 import eComProject.NOVELoPEDIABackEnd.dto.Category;
 
-
 public class CategoryTestCase {
 
 	private static AnnotationConfigApplicationContext context;
-	
-	
-	
+
 	private static CategoryDAO categoryDAO;
-	
-	
-	private  Category category;
-	
-	
-	
+
+	private Category category;
+
 	@BeforeClass
-	public static void init(){
+	public static void init() {
 		context = new AnnotationConfigApplicationContext();
 		context.scan("eComProject.NOVELoPEDIABackEnd");
 		context.refresh();
-		
-		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");
+
+		categoryDAO = (CategoryDAO) context.getBean("categoryDAO");
 	}
-	
-	
+
+	/*
+	 * @Test public void testAddCategory() {
+	 * 
+	 * category = new Category();
+	 * 
+	 * category.setName("Romance");
+	 * category.setDescription("The Romance Genre");
+	 * category.setImageURL("Romance1.png");
+	 * 
+	 * assertEquals("Added Category", true, categoryDAO.add(category));
+	 * 
+	 * }
+	 */
+	/*
+	 * @Test public void testGetCategory(){
+	 * 
+	 * category = categoryDAO.get(3);
+	 * assertEquals("Successfully Fetched a Single Category from the table",
+	 * "Fiction", category.getName());
+	 * 
+	 * }
+	 */
+	/*
+	 * @Test public void testUpdateCategory(){
+	 * 
+	 * category = categoryDAO.get(3); category.setName("FIc");
+	 * assertEquals("Successfully Updated a Single Category from the table",
+	 * true, categoryDAO.update(category));
+	 * 
+	 * }
+	 */
+	/*
+	 * @Test public void testDeleteCategory(){
+	 * 
+	 * category = categoryDAO.get(4);
+	 * assertEquals("Successfully deleted a Single Category from the table",
+	 * true, categoryDAO.delete(category));
+	 * 
+	 * }
+	 */
+	/*
+	 * @Test public void testListCategory(){
+	 * 
+	 * assertEquals("Successfully fetched List of Categories from the table", 3,
+	 * categoryDAO.list().size());
+	 * 
+	 * }
+	 */
+
 	@Test
-	public void testAddCategory(){
+	public void testCRUDCategory() {
+
+		// addOperation
+		category = new Category();
+
+		category.setName("Romance");
+		category.setDescription("The Romance Genre");
+		category.setImageURL("Romance1.png");
+		assertEquals("Added Category", true, categoryDAO.add(category));
+		
 		
 		category = new Category();
-		
-		category.setName("Lap");
-		category.setDescription("This is some discription_1");
-		category.setImageURL("cat_1.png");
-		
-		assertEquals("Added Category",true,categoryDAO.add(category));
-		
+
+		category.setName("Fiction");
+		category.setDescription("The Fiction Genre");
+		category.setImageURL("Fiction1.png");
+		assertEquals("Added Category", true, categoryDAO.add(category));
+
+		// fetching and Updating
+		category = categoryDAO.get(2);
+		category.setName("Fic");
+		assertEquals("Successfully Updated a Single Category from the table", true, categoryDAO.update(category));
+
+		// fetching and deleting the category
+		assertEquals("Successfully deleted a Single Category from the table", true, categoryDAO.delete(category));
+
+		// fetching a list
+		assertEquals("Successfully fetched List of Categories from the table", 1, categoryDAO.list().size());
 
 	}
+
 }
